db06be6e3b6035a36cdb45e387448071ec268efc
Priyabas1404 <109682693+Priyabas1404@users.noreply.github.com>
2024-10-03 11:51:22 -0500

    AlarmBroadcastReceiver create, update and cancel

---
+++ app/src/main/java/com/example/alarmbroadcastreceiverbasic/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.alarmbroadcastreceiverbasic
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.alarmbroadcastreceiverbasic.databinding.ActivityMainBinding
+import java.util.Date
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var context: Context
+    private lateinit var alarmManager: AlarmManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        context = this
+        alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        binding.buttonCreate.setOnClickListener{
+            val seconds = binding.timer.text.toString().toInt() * 1000
+            val intent = Intent(context, Receiver::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            Log.d("MainActivity", "Create : " + Date().toString())
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + seconds, pendingIntent)
+        }
+
+        binding.buttonUpdate.setOnClickListener{
+            val seconds = binding.timer.text.toString().toInt() * 1000
+            val intent = Intent(context, Receiver::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            Log.d("MainActivity", "Update : " + Date().toString())
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + seconds, pendingIntent)
+        }
+
+        binding.buttonCancel.setOnClickListener{
+            val intent = Intent(context, Receiver::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            Log.d("MainActivity", "Cancel : " + Date().toString())
+            alarmManager.cancel(pendingIntent)
+        }
+
+    }
+
+    class Receiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            Log.d("MainActivity", "Receiver : " + Date().toString())
+        }
+    }
+}
