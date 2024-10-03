d944321ffef7f5a775da96b9ea7e06eaba483d56
Priyabas1404 <109682693+Priyabas1404@users.noreply.github.com>
2024-10-03 11:53:31 -0500

    AlarmBroadcastReceiver create, update and cancel

--- app/src/main/java/com/example/alarmbroadcastreceiverbasic/MainActivity.kt
+++ app/src/main/java/com/example/alarmbroadcastreceiverbasic/MainActivity.kt
@@ -7,14 +7,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.alarmbroadcastreceiverbasic.databinding.ActivityMainBinding
 import java.util.Date
 
